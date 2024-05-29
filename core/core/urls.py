a946a4cb0d5cf4da07dddfcc3097ed456a62494a
iliya-askari <elyaaskarii86@gmail.com>
2024-05-29 23:22:05 +0430

    add login and logout url and static file

--- core/core/urls.py
+++ core/core/urls.py
@@ -18,10 +18,15 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('accounts/login/',views.LoginView.as_view()),
+    path('accounts/logout/', views.LogoutView.as_view()),
+    
 ]
 
 # serving static and mdia for development
