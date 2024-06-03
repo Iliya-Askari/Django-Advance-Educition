9df8e0a290a94038e86efb1437cd73dde95512af
iliya-askari <elyaaskarii86@gmail.com>
2024-06-04 00:31:39 +0430

    status code serializer 404

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from blog.models import Post
+from rest_framework import status
 
 @api_view()
 def post_list(request):
@@ -10,6 +11,9 @@ def post_list(request):
 
 @api_view()
 def post_detail(request,id):
-    post = Post.objects.get(pk=id)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+    try:
+        post = Post.objects.get(pk=id)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    except Post.DoesNotExist:
+        return Response({"detail":"post not found"},status=status.HTTP_404_NOT_FOUND)
