6d5a9505cce7fd079c9c1849677e0abef9080086
iliya-askari <elyaaskarii86@gmail.com>
2024-06-04 00:20:03 +0430

    create base serializer file

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from blog.models import Post
 
 @api_view()
 def post_list(request):
     return Response('ok')
+
+@api_view()
+def post_detail(request,id):
+    post = Post.objects.get(pk=id)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
