03930c7973cb85d34937a98bbdc1eec626052315
iliya-askari <elyaaskarii86@gmail.com>
2024-06-04 00:46:04 +0430

    resume post list show in rest_freamwork

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -8,7 +8,9 @@ from django.shortcuts import get_object_or_404
 
 @api_view()
 def post_list(request):
-    return Response('ok')
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def post_detail(request,id):
