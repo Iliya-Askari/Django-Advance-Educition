1df3bd66a0762ac35afdd36f1739cb158c6ca789
iliya-askari <elyaaskarii86@gmail.com>
2024-06-07 00:29:35 +0430

    add url path api-auth in browser

--- core/core/urls.py
+++ core/core/urls.py
@@ -22,6 +22,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('blog/', include('blog.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
     path('accounts/login/',views.LoginView.as_view()),
