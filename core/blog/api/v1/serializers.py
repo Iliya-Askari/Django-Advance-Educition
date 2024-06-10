9d514d9a82763381f05ccb4e2344dbfa8b8b4276
iliya-askari <elyaaskarii86@gmail.com>
2024-06-11 00:05:37 +0430

    add and traning reanonly fildes

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -8,9 +8,12 @@ class PostSerializer(serializers.ModelSerializer):
     '''
     this is a class to define posts for blog app
     '''
+    # content = serializers.ReadOnlyField()
+    # content = serializers.CharField(read_only=True)
     class Meta:
         model = Post
         fields = ['id','author', 'title', 'content', 'status', 'created_date', 'published_date']
+        read_only_fields = ['content',]
 
 class CtegorySerializer(serializers.ModelSerializer):
     class Meta:
