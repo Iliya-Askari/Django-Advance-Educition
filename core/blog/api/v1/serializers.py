0e09729440bc222debd53d264ac136a1e0fd026e
iliya-askari <elyaaskarii86@gmail.com>
2024-06-12 17:12:51 +0430

    Displaying categories in two ways in the post creation and post display sections

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -4,6 +4,10 @@ from blog.models import Post , Category
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
 
+class CtegorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id','name']
 class PostSerializer(serializers.ModelSerializer):
     '''
     this is a class to define posts for blog app
@@ -11,17 +15,22 @@ class PostSerializer(serializers.ModelSerializer):
     # content = serializers.ReadOnlyField()
     # content = serializers.CharField(read_only=True)
     snippet = serializers.ReadOnlyField(source='get_snippet')
-    relaitive_url = serializers.URLField(source="get_absolute_api_url")
+    # relaitive_url = serializers.URLField(source="get_absolute_api_url")
     absolute_url = serializers.SerializerMethodField()
+    # category = serializers.SlugRelatedField(many=False, slug_field='name',queryset=Category.objects.all())
     class Meta:
         model = Post
-        fields = ['id','author', 'title', 'content','snippet', 'status','relaitive_url', 'absolute_url','created_date', 'published_date']
-        read_only_fields = ['content',]
+        fields = ['id','author', 'title', 'category','content','snippet', 'status', 'absolute_url','created_date', 'published_date']
+        # read_only_fields = ['content',]
+
     def get_absolute_url(self,obj):
+        '''
+        Get the full address of the posts
+        '''
         request = self.context.get('request')
         return request.build_absolute_uri(obj.id)
-
-class CtegorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id','name']
+    
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep ['category'] = CtegorySerializer(instance.category).data
+        return rep
