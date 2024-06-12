8aa33aed7a9e7a1ee1410720d677d8e10e61bcf0
iliya-askari <elyaaskarii86@gmail.com>
2024-06-12 17:19:52 +0430

    Update serializers.py

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -5,6 +5,9 @@ from blog.models import Post , Category
 #     title = serializers.CharField(max_length=255)
 
 class CtegorySerializer(serializers.ModelSerializer):
+    '''
+    this is a class to define category for blog app
+    '''
     class Meta:
         model = Category
         fields = ['id','name']
@@ -12,12 +15,15 @@ class PostSerializer(serializers.ModelSerializer):
     '''
     this is a class to define posts for blog app
     '''
+
+    # Testing different read-only methods in the serializer
     # content = serializers.ReadOnlyField()
     # content = serializers.CharField(read_only=True)
-    snippet = serializers.ReadOnlyField(source='get_snippet')
     # relaitive_url = serializers.URLField(source="get_absolute_api_url")
-    absolute_url = serializers.SerializerMethodField()
     # category = serializers.SlugRelatedField(many=False, slug_field='name',queryset=Category.objects.all())
+
+    absolute_url = serializers.SerializerMethodField()
+    snippet = serializers.ReadOnlyField(source='get_snippet')
     class Meta:
         model = Post
         fields = ['id','author', 'title', 'category','content','snippet', 'status', 'absolute_url','created_date', 'published_date']
@@ -31,6 +37,9 @@ class PostSerializer(serializers.ModelSerializer):
         return request.build_absolute_uri(obj.id)
     
     def to_representation(self, instance):
+        '''
+        With this method, you can change the method of displaying data in different parts
+        '''
         rep = super().to_representation(instance)
         rep ['category'] = CtegorySerializer(instance.category).data
         return rep
