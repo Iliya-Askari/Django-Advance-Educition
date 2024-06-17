d7b2ca557649884eb4acf2f583396f3c5e93fb1b
iliya-askari <elyaaskarii86@gmail.com>
2024-06-17 23:40:32 +0430

    setup api files in accounts app

---
+++ core/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path , include
+from django.contrib.auth import views
+
+app_name='accounts'
+
+urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
+    path('login/',views.LoginView.as_view()),
+    path('logout/', views.LogoutView.as_view()),
+    path("api/v1/",include('accounts.api.v1.urls'))
+]
