c47b66da3d5e9288e633718170609036d589face
iliya-askari <elyaaskarii86@gmail.com>
2024-06-18 23:10:39 +0430

    add url login

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path , include
+# from rest_framework.authtoken.views import ObtainAuthToken
 from . import views
+
 app_name='api-v1'
 
 
 urlpatterns = [
     # regestrations
-    path('registrations/',views.RegistrationsApiView.as_view(),name='registrations')
+    path('registrations/',views.RegistrationsApiView.as_view(),name='registrations'),
+    path('token/login/',views.CustomObtainAuthToken.as_view(),name='token-login'),
     # login token
     # login jwt
     # change password
