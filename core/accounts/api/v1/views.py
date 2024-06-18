1ded550c0994c5e6fcde72d210eb98cc2e6a0aae
iliya-askari <elyaaskarii86@gmail.com>
2024-06-18 23:09:56 +0430

    create coustoum views token in login

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics
-from .serializers import RegistrationsSerializer
+from .serializers import RegistrationsSerializer,CoustoumAuthTokenSerializer
+
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+
 class RegistrationsApiView(generics.GenericAPIView):
     serializer_class = RegistrationsSerializer
     
@@ -15,4 +19,19 @@ class RegistrationsApiView(generics.GenericAPIView):
             return Response(data , status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    serializer_class = CoustoumAuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
     
