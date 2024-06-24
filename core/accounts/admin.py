cb6274b194bae7fdca9834583949cba3e0209efd
iliya-askari <elyaaskarii86@gmail.com>
2024-06-25 01:07:02 +0430

    add fields verified in model and serializer

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -5,8 +5,8 @@ from .models import User , Profile
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('email','is_superuser','is_active')
-    list_filter = ('email','is_superuser','is_active')
+    list_display = ('email','is_superuser','is_active','is_verified')
+    list_filter = ('email','is_superuser','is_active','is_verified')
     search_fields = ('email',)
     ordering = ('email',)
     fieldsets = (
@@ -17,7 +17,7 @@ class CustomUserAdmin(UserAdmin):
         }),
         ('Permissions' , {
             "fields": (
-                'is_staff','is_active','is_superuser'
+                'is_staff','is_active','is_superuser','is_verified'
             ),
         }),
         ('group permissions' , {
@@ -35,7 +35,7 @@ class CustomUserAdmin(UserAdmin):
         (None, {
             "classes": ("wide",),
             "fields": (
-                "email", "password1","password2", "is_staff","is_active", "is_superuser"
+                "email", "password1","password2", "is_staff","is_active", "is_superuser","is_verified"
             )}
         ),
     )
