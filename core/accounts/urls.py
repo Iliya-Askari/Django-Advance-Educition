8ec222d0610145fbc5f8833a943162ab0c1c6571
iliya-askari <elyaaskarii86@gmail.com>
2024-07-14 00:57:50 +0430

    create base tasks for celery

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from django.contrib.auth import views
 
+from . import views as view
+
 app_name = "accounts"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("login/", views.LoginView.as_view()),
     path("logout/", views.LogoutView.as_view()),
+    path("send-email/", view.send_email, name='send-email'),
     path("api/v1/", include("accounts.api.v1.urls")),
     path("api/v2/", include("djoser.urls")),
     path("api/v2/", include("djoser.urls.jwt")),
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import time
 
+from .tasks import send_emai
 # Create your views here.
+
+def send_email(request):
+    send_emai.delay()
+    return HttpResponse("<h1>Email sent successfully</h1>")
