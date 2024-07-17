78b15821ac4d44d9b6e070a712972f0271c4eaed
iliya-askari <elyaaskarii86@gmail.com>
2024-07-18 02:54:08 +0430

    Update views.py

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse ,JsonResponse
 import time
 import requests
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 
 from .tasks import send_emai
 # Create your views here.
@@ -10,6 +12,7 @@ def send_email(request):
     send_emai.delay()
     return HttpResponse("<h1>Email sent successfully</h1>")
 
+@cache_page(60)
 def test(request):
     response  = requests.get('https://56265dcd-4ee7-4c3b-b7d4-bc84d1bbe1b5.mock.pstmn.io/test/delay/5')
     return JsonResponse(response.json())
